0a52f02102491965bde9d3b36de76aaef714370d
SigeR <SigeR@SigeR-PC>
2016-11-18 18:11:19 -0200

    Scanner add to try

--- Caelum/src/br/com/caelum/c15IO/TesteScanner2.java
+++ Caelum/src/br/com/caelum/c15IO/TesteScanner2.java
@@ -7,9 +7,9 @@ import java.util.Scanner;
 public class TesteScanner2 {
 	public static void main(String[] args) {
 		String arquivo = "TesteIO/Teste1.txt";
-		Scanner s = new Scanner(System.in);
+		
 
-		try(PrintStream ps = new PrintStream(arquivo)) {
+		try(Scanner s = new Scanner(System.in); PrintStream ps = new PrintStream(arquivo)) {
 			while(s.hasNextLine()) {
 				ps.println(s.nextLine());
 			}
